ff1ade70d4d918f82671c17d4e09f034c6616ad8
CaptM44 <mmm211@zips.uakron.edu>
2017-09-27 00:34:22 -0400

    scores in

--- src/web-live/script.js
+++ src/web-live/script.js
@@ -2,58 +2,48 @@ $(function () {
 
     api.sport = 'baseball';
     api.league = 'mlb';
-    api.teamId = 3;
+    api.teamId = api.getMlbTeamId('SF');
 
     api.getTodaysGame(todaysGame => {
         if (todaysGame) {
-
             var gameId = todaysGame.id;
-            api.getTeamInfo(todaysGame.homeTeam.id, t => renderTeamInfo($('#HomeTeam'), t));
-            api.getTeamInfo(todaysGame.awayTeam.id, t => renderTeamInfo($('#AwayTeam'), t));
-            var home = todaysGame.homeTeam.abbreviation;
-            var away = todaysGame.awayTeam.abbreviation;
+            api.getTeamInfo(todaysGame.homeTeam.id, t => renderTeamInfo(t, true));
+            api.getTeamInfo(todaysGame.awayTeam.id, t => renderTeamInfo(t, false));
 
+            api.getScoreUpdate(gameId, gameState => renderGameState(gameState));
             setInterval(() => {
-                var time = new Date().toJSON().replace(/-|T|:/g, '').substr(0, 14);
-                var url = 'https://api.foxsports.com/sportsdata/v1/live/' + league + '/scores.json?date=' + date + '&t=' + time + '&apikey=' + apiKey;
-                $.get(url).done(games => {
-
-                    var gameState = games.find(t => t.GameState.GameId == gameId).GameState;
-                    var game = {
-                        inning: (gameState.IsInningTop ? 'Top ' : 'Bottom ') + gameState.Inning,
-                        outs: gameState.Outs,
-                        home: {
-                            score: gameState.HomeScore,
-                            abbreviation: todaysGame.homeTeam.abbreviation,
-                            logUrl: todaysGame.homeTeam.abbreviation,
-                        },
-                        awayScore: gameState.awayScore,
-                    }
-
-                    api.updateTicker(game);
-                    // var text = '';
-                    // text += home + ': ' + game.HomeScore + ', ';
-                    // text += away + ': ' + game.AwayScore;
-                    // if (sport == 'baseball') {
-                    //     text += '  ' + (game.IsInningTop ? 'TOP' : 'BOT');
-                    //     text += game.Inning;
-                    //     text += '  Balls:' + game.Balls;
-                    //     text += '  Strikes:' + game.Strikes;
-                    //     text += '  Outs:' + game.Outs;
-                    //     text += '    ';
-                    //     text += (game.RunnerOn1BId ? 'first, ' : '');
-                    //     text += (game.RunnerOn2BId ? 'second, ' : '');
-                    //     text += (game.RunnerOn3BId ? 'third, ' : '');
-                    // }
-                    // console.log(text)
-                });
-            }, 2000);
+                api.getScoreUpdate(gameId, gameState => renderGameState(gameState));
+            }, 5000);
         }
     });
 });
 
-function renderTeamInfo(selector, team) {
+function renderTeamInfo(team, isHome) {
+    var selector = isHome ? $('#HomeTeam') : $('#AwayTeam');
     selector.find('.logo').html('<img src="' + team.links.logos.Medium + '">');
     selector.find('.abbr').html(team.abbreviation);
     selector.find('.record').html(team.record.wins + ' - ' + team.record.losses);
+}
+
+function renderGameState(gameState) {
+    if (gameState.Status == 'Final') {
+        $('#Line1').html('')
+        if (gameState.HomeScore > gameState.AwayScore) {
+            $('#AwayScore').addClass('faded');
+            $('#Line2').html('Final <i class="fa fa-caret-right"></i>');
+        }
+        else {
+            $('#HomeScore').addClass('faded');
+            $('#Line2').html('<i class="fa fa-caret-left"></i> Final');
+        }
+    }
+    else {
+        var suffix = ['', 'st', 'nd', 'rd'][gameState.Inning] || 'th';
+        var desc = gameState.IsActive ? (gameState.IsInningTop ? 'Top ' : 'Bottom ') : (gameState.IsInningTop ? 'End ' : 'Middle ');
+        $('#Line1').html(desc + gameState.Inning + suffix)
+        $('#Line2').html(gameState.IsActive ? (gameState.Outs + (gameState.Outs == 1 ? ' out' : ' outs')) : '');
+    }
+
+    $('#HomeScore').html(gameState.HomeScore);
+    $('#AwayScore').html(gameState.AwayScore);
 }
