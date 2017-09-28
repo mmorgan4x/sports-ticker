75acf328af03d66268f61943ac406007affaa77e
CaptM44 <mmm211@zips.uakron.edu>
2017-09-27 20:50:39 -0400

    pre game handled

--- src/web-live/script.js
+++ src/web-live/script.js
@@ -1,22 +1,48 @@
 $(function () {
 
-    api.sport = 'baseball';
-    api.league = 'mlb';
-    api.teamId = api.getMlbTeamId('HOU');
+    api.sport = getParameterByName('sport') || 'baseball';
+    api.league = getParameterByName('league') || 'mlb';
+    api.teamId = api.getMlbTeamId(getParameterByName('team') || 'CLE');
 
-    api.getTodaysGame(todaysGame => {
-        if (todaysGame) {
-            var gameId = todaysGame.id;
-            api.getTeamInfo(todaysGame.homeTeam.id, t => renderTeamInfo(t, true));
-            api.getTeamInfo(todaysGame.awayTeam.id, t => renderTeamInfo(t, false));
+    getGame();
+    function getGame() {
+        api.getTodaysGame(todaysGame => {
+            if (todaysGame) {
+                var gameId = todaysGame.id;
+                var gameStart = new Date(todaysGame.date);
+                var status = todaysGame.status.name;
 
-            api.getScoreUpdate(gameId, gameState => renderGameState(gameState));
-            setInterval(() => {
-                api.getScoreUpdate(gameId, gameState => renderGameState(gameState));
-            }, 5000);
-        }
-    });
+                clearGameState();
+                api.getTeamInfo(todaysGame.homeTeam.id, t => renderTeamInfo(t, true));
+                api.getTeamInfo(todaysGame.awayTeam.id, t => renderTeamInfo(t, false));
+
+                if (status == 'Pregame') {
+                    renderGameStart(gameStart);
+                    setTimeout(getGame, 10000);
+                }
+                else {
+                    updateGame();
+                    var gameStateInterval = setInterval(() => updateGame, 5000);
+
+                    function updateGame() {
+                        api.getScoreUpdate(gameId, gameState => {
+                            renderGameState(gameState);
+                            if (gameState.Status == 'Final') {
+                                clearInterval(gameStateInterval);
+                                setTimeout(getGame, 10000);
+                            }
+                        })
+                    }
+                }
+            }
+        });
+    }
 });
+
+function clearGameState() {
+    $('#HomeScore, #AwayScore').html('').removeClass('faded');
+    $('#Line1, #Line2').html('');
+}
 
 function renderTeamInfo(team, isHome) {
     var selector = isHome ? $('#HomeTeam') : $('#AwayTeam');
@@ -25,8 +51,25 @@ function renderTeamInfo(team, isHome) {
     selector.find('.record').html(team.record.wins + ' - ' + team.record.losses);
 }
 
+function renderGameStart(time) {
+    $('#Line1').html(time.toLocaleDateString())
+    $('#Line2').html(formatTime(time));
+}
+
 function renderGameState(gameState) {
-    if (gameState.Status == 'Final') {
+    if (gameState.Status == 'In Progress') {
+        $('#HomeScore').html(gameState.HomeScore);
+        $('#AwayScore').html(gameState.AwayScore);
+
+        var suffix = ['', 'st', 'nd', 'rd'][gameState.Inning] || 'th';
+        var desc = gameState.IsActive ? (gameState.IsInningTop ? 'Top ' : 'Bottom ') : (gameState.IsInningTop ? 'End ' : 'Middle ');
+        $('#Line1').html(desc + gameState.Inning + suffix)
+        $('#Line2').html(gameState.IsActive ? (gameState.Outs + (gameState.Outs == 1 ? ' out' : ' outs')) : '');
+    }
+    else if (gameState.Status == 'Final') {
+        $('#HomeScore').html(gameState.HomeScore);
+        $('#AwayScore').html(gameState.AwayScore);
+
         $('#Line1').html('')
         if (gameState.HomeScore > gameState.AwayScore) {
             $('#AwayScore').addClass('faded');
@@ -37,13 +80,21 @@ function renderGameState(gameState) {
             $('#Line2').html('<i class="fa fa-caret-left"></i> Final');
         }
     }
-    else {
-        var suffix = ['', 'st', 'nd', 'rd'][gameState.Inning] || 'th';
-        var desc = gameState.IsActive ? (gameState.IsInningTop ? 'Top ' : 'Bottom ') : (gameState.IsInningTop ? 'End ' : 'Middle ');
-        $('#Line1').html(desc + gameState.Inning + suffix)
-        $('#Line2').html(gameState.IsActive ? (gameState.Outs + (gameState.Outs == 1 ? ' out' : ' outs')) : '');
-    }
+}
 
-    $('#HomeScore').html(gameState.HomeScore);
-    $('#AwayScore').html(gameState.AwayScore);
+
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function formatTime(d) {
+    function z(n) { return (n < 10 ? '0' : '') + n }
+    var h = d.getHours();
+    return (h % 12 || 12) + ':' + z(d.getMinutes()) + ' ' + (h < 12 ? 'AM' : 'PM');
 }
