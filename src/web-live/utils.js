10592e5fbc8b245abc2732f2c1c5abc7bdc50abe
CaptM44 <mmm211@zips.uakron.edu>
2017-10-02 15:31:27 -0400

    time bug

--- src/web-live/utils.js
+++ src/web-live/utils.js
@@ -10,6 +10,7 @@ var Utils = {
     },
 
     formatTime(d) {
+        return d.toLocaleTimeString();
         function z(n) { return (n < 10 ? '0' : '') + n }
         var h = d.getHours();
         return (h % 12 || 12) + ':' + z(d.getMinutes()) + ' ' + (h < 12 ? 'AM' : 'PM');
