53de8c0af03b4835324abdb1c23ba3713240373a
Matt Morgan <mmm211@zips.uakron.edu>
2018-03-15 23:10:17 -0400

    commit

--- src/node-test/main.ts
+++ src/node-test/main.ts
@@ -1,14 +1,21 @@
-import { Gpio } from 'onoff';
+import * as http from 'request-promise';
 
+const apiKey = 'jE7yBJVRNAwdDesMgTzTXUUSx1It41Fq';
 console.log('starting...');
 
-let led = new Gpio(17, 'out');
-let state = false;
-
 (async () => {
-  setInterval(t => {
-    state = !state;
-    led.writeSync(state ? 1 : 0);
-    console.log(state ? 'onn' : 'offf')
-  }, 1000)
+  for (let i = 1; i < 33; i++) {
+    try {
+      let res = JSON.parse(await http.get(getTeamInfo(i)))
+      console.log(res.location);
+    }
+    catch {
+      console.log('err');
+    }
+  }
 })();
+
+
+function getTeamInfo(id: number) {
+  return `https://api.foxsports.com/sportsdata/v1/basketball/nba/teams/${id}.json?&apikey=${apiKey}`;
+}
