f2b1c26ce75b0b849fdc057af4025d21b612f2ba
Matt Morgan <mmm211@zips.uakron.edu>
2018-03-22 21:31:30 -0400

    changes to serial-com still WIP

--- src/arduino/serial-com/serial-com.ino
+++ src/arduino/serial-com/serial-com.ino
@@ -4,13 +4,19 @@ void setup() {
 }
 
 String serialReadBuffer = "";
+String event = "";
+String arg0 = "";
+String arg1 = "";
+String arg2 = "";
+String arg3 = "";
 
 void loop() {
   if (Serial.available() > 0) {
     char character = Serial.read();
 
     if (character == '\n') {
-      runCommand(serialReadBuffer);
+      setParams(serialReadBuffer);
+      runCommand();
       serialReadBuffer = "";
     }
     else {
@@ -19,89 +25,46 @@ void loop() {
   }
 }
 
-void runCommand(String msg) {
-
-  String* args = deserialize(msg);
-  String event = args[0];
-  String arg0 = args[1];
-  String arg1 = args[2];
-  String arg2 = args[3];
-
-  String retEvent = "";
-  String retArgs[10] = {};
-
+void runCommand() {
   if (event == "ping") {
-    retEvent = "pong";
+    emit("pong", "", "", "", "");
   }
   if (event == "digitalWrite") {
     digitalWrite(arg0.toInt(), arg1 == "LOW" ? LOW : HIGH);
-    retEvent = "digitalWrite";
-    retArgs[0] = arg0;
-    retArgs[1] = arg1;
+    emit("digitalWrite", arg0, arg1, "", "");
   }
-
-  String retMsg = serialize(retEvent, retArgs);
-  write(retMsg);
-  delete[] args;
 }
 
-
-
-void write(String msg) {
-  Serial.print(msg + '\n');
-}
-
-void write(String args[]) {
-  String msg = "";
-  for (int i = 0; i < getArraySize(args); i++) {
-    msg += (i == 0 ? "" : "...") + args[i];
-  }
-  Serial.print(msg + '\n');
-}
-
-String serialize(String event, String args[]) {
+void emit(String event, String rArg0 , String rArg1, String rArg2, String rArg3 ) {
   String msg = event + ":";
-  for (int i = 0; i < getArraySize(args); i++) {
-    msg += (i == 0 ? "" : ",") + args[i];
-  }
-  return msg;
+
+  msg += (rArg0 == NULL ? "" : ("," + rArg0));
+  msg += (rArg1 == NULL ? "" : ("," + rArg1));
+  msg += (rArg2 == NULL ? "" : ("," + rArg2));
+  msg += (rArg3 == NULL ? "" : ("," + rArg3));
+
+  Serial.print(msg + '\n');
 }
 
-String* deserialize(String msg) {
-  String* args = new String[10];
-  int insertIndex = 0;
-
-  //event
-  args[insertIndex] = msg.substring(0, msg.indexOf(':'));
-  insertIndex++;
-
+void setParams(String msg) {
+  String args[4];
   String argsStr = msg.substring(msg.indexOf(":") + 1, msg.length());
-  String temp = "";
 
-  //args
+  int argIndex = 0;
   for (int i = 0; i < argsStr.length(); i++) {
     if (argsStr[i] == ',') {
-      args[insertIndex] = temp;
-      insertIndex++;
-      temp = "";
+      argIndex++;
     }
     else {
-      temp += argsStr[i];
+      args[argIndex] += argsStr[i];
     }
   }
-  if (temp != NULL) {
-    args[insertIndex] = temp;
-  }
 
-  return args;
-}
-
-int getArraySize(String arr[]) {
-  int size = 0;
-  while (size < 10 && arr[size] != NULL) {
-    size++;
-  }
-  return  size;
+  event = msg.substring(0, msg.indexOf(':'));
+  arg0 = args[0];
+  arg1 = args[1];
+  arg2 = args[2];
+  arg3 = args[3];
 }
 
 
